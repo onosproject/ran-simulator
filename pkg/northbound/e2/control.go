90c652fbd2ad615dfdba622104b91ce535f39a07
Sean Condon <sean@opennetworking.org>
2020-04-21 14:04:57 +0100

    Relieve deadlock condition when loaded with 300 UEs

--- pkg/northbound/e2/control.go
+++ pkg/northbound/e2/control.go
@@ -50,7 +50,7 @@ func ricControlResponse(port int, stream e2ap.E2AP_RicChanServer, c chan e2ap.Ri
 	for {
 		select {
 		case msg := <-c:
-			UpdateTelemetryMetrics(&msg)
+			go UpdateTelemetryMetrics(&msg)
 			if err := stream.Send(&msg); err != nil {
 				log.Infof("send error %v", err)
 				return err
@@ -151,10 +151,10 @@ func handleCellConfigRequest(port int, ecgi types.ECGI, c chan e2ap.RicIndicatio
 
 	trafficSimMgr := manager.GetManager()
 	trafficSimMgr.TowersLock.RLock()
-	defer trafficSimMgr.TowersLock.RUnlock()
 	tower, ok := trafficSimMgr.Towers[ecgi]
 	if !ok {
 		log.Warnf("Tower %s not found for handleCellConfigRequest on Port %d", ecgi, port)
+		trafficSimMgr.TowersLock.RUnlock()
 		return
 	}
 	cells := make([]*e2.CandScell, 0, 8)
@@ -166,6 +166,7 @@ func handleCellConfigRequest(port int, ecgi types.ECGI, c chan e2ap.RicIndicatio
 		}
 		cells = append(cells, &cell)
 	}
+	trafficSimMgr.TowersLock.RUnlock()
 	e2Ecgi := toE2Ecgi(tower.Ecgi)
 	cellConfigReport := e2ap.RicIndication{
 		Hdr: &e2sm.RicIndicationHeader{
