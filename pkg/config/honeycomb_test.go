9dd90453e5f72dd00ea36cab032f794833c632bb
Thomas Vachuska <thomas.vachuska@gmail.com>
2020-06-18 11:23:20 -0700

    Switch to ONF member license. (#145)

--- pkg/config/honeycomb_test.go
+++ pkg/config/honeycomb_test.go
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
 //
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
 //
 
 package config
