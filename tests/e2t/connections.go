9ef4a2e3514c2e3f5dccfc2e5e8e5c1ddb7baca6
adibrastegarnia <adib.rastegarnia@gmail.com>
2021-01-21 16:24:37 -0800

    Fix linter

--- tests/e2t/connections.go
+++ tests/e2t/connections.go
@@ -12,12 +12,10 @@ import (
 	"github.com/onosproject/ran-simulator/tests/utils"
 )
 
-// TestConnections
+// TestConnections test connectivity between e2 agents and e2t
 func (s *TestSuite) TestConnections(t *testing.T) {
 	// Creates an instance of the simulator
 	simulator := utils.CreateRanSimulatorWithName(t, "ran-simulator")
-	simulator.Set("model.nodes.node2.ecgi", "90125-10003")
-
 	err := simulator.Install(true)
 	assert.NoError(t, err, "could not install device simulator %v", err)
 
