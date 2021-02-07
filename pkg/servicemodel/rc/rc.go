fb89bda706cd0b30982ec855eb04295fcd58d5d5
adibrastegarnia <adib.rastegarnia@gmail.com>
2021-02-07 13:03:46 -0800

    Remove hard coded values for ran function descriptions

--- pkg/servicemodel/rc/rc.go
+++ pkg/servicemodel/rc/rc.go
@@ -49,7 +49,7 @@ func NewServiceModel(node model.Node, model *model.Model, modelPluginRegistry *m
 		Model:               model,
 	}
 
-	var ranFunctionShortName = "ORAN-E2SM-RC"
+	var ranFunctionShortName = string(modelFullName)
 	var ranFunctionE2SmOid = "OID124"
 	var ranFunctionDescription = "RC PRE"
 	var ranFunctionInstance int32 = 3
@@ -58,8 +58,8 @@ func NewServiceModel(node model.Node, model *model.Model, modelPluginRegistry *m
 	var ricEventFormatType int32 = 1
 	var ricReportStyleType int32 = 1
 	var ricReportStyleName = "PCI and NRT update for eNB"
-	var ricIndicationHeaderFormatType int32 = 21
-	var ricIndicationMessageFormatType int32 = 56
+	var ricIndicationHeaderFormatType int32 = 1
+	var ricIndicationMessageFormatType int32 = 1
 	ranFuncDescPdu, err := pdubuilder.CreateE2SmRcPreRanfunctionDescriptionMsg(ranFunctionShortName, ranFunctionE2SmOid, ranFunctionDescription,
 		ranFunctionInstance, ricEventStyleType, ricEventStyleName, ricEventFormatType, ricReportStyleType, ricReportStyleName,
 		ricIndicationHeaderFormatType, ricIndicationMessageFormatType)
@@ -80,12 +80,11 @@ func NewServiceModel(node model.Node, model *model.Model, modelPluginRegistry *m
 		log.Info("model plugin names:", modelPluginRegistry.ModelPlugins)
 		return registry.ServiceModel{}, errors.New(errors.Invalid, "model plugin is nil")
 	}
-	// TODO it panics and it should be fixed in rc service model otherwise it panics
-	/*ranFuncDescBytes, err := rcModelPlugin.RanFuncDescriptionProtoToASN1(protoBytes)
+	ranFuncDescBytes, err := rcModelPlugin.RanFuncDescriptionProtoToASN1(protoBytes)
 	if err != nil {
 		log.Error(err)
 		return registry.ServiceModel{}, err
-	}*/
+	}
 
 	rcSm.Description = ranFuncDescBytes
 
@@ -111,13 +110,4 @@ func (sm Client) RICSubscriptionDelete(ctx context.Context, request *e2appducont
 	log.Info("Ric subscription delete request is received for service model:", sm.ServiceModel.ModelFullName)
 	// TODO implements handler for subscription delete requests
 	return response, failure, err
-}
-
-var ranFuncDescBytes = []byte{
-	0x20, 0xf0, 0x4f, 0x52, 0x41, 0x4e, 0x2d, 0x45, 0x32, 0x53, 0x4d, 0x2d, 0x52, 0x43, 0x2d, 0x50, 0x52,
-	0x45, 0x00, 0x00, 0x05, 0x4f, 0x49, 0x44, 0x31, 0x32, 0x33, 0x02, 0x80, 0x52, 0x43, 0x20, 0x50, 0x52,
-	0x45, 0x01, 0x03, 0x60, 0x00, 0x01, 0x01, 0x07, 0x00, 0x50, 0x65, 0x72, 0x69, 0x6f, 0x64, 0x69, 0x63,
-	0x20, 0x72, 0x65, 0x70, 0x6f, 0x72, 0x74, 0x01, 0x05, 0x00, 0x01, 0x01, 0x0c, 0x80, 0x50, 0x43, 0x49,
-	0x20, 0x61, 0x6e, 0x64, 0x20, 0x4e, 0x52, 0x54, 0x20, 0x75, 0x70, 0x64, 0x61, 0x74, 0x65, 0x20, 0x66,
-	0x6f, 0x72, 0x20, 0x65, 0x4e, 0x42, 0x01, 0x01, 0x01, 0x01,
 }
