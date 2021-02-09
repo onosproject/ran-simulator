ea9b0e16f8b7da2db2d8b6a69286b5ad81092c73
Ray Milkey <ray@opennetworking.org>
2021-02-09 08:42:13 -0800

    Allow specifying repo credentials for fetching sdran charts (#208)

--- tests/utils/sdran.go
+++ tests/utils/sdran.go
@@ -5,14 +5,37 @@ package utils
 
 import (
 	"github.com/onosproject/helmit/pkg/helm"
+	"github.com/onosproject/helmit/pkg/kubernetes"
 	"github.com/onosproject/helmit/pkg/util/random"
 	"github.com/onosproject/onos-test/pkg/onostest"
 )
 
+func getCredentials() (string, string, error) {
+	kubClient, err := kubernetes.New()
+	if err != nil {
+		return "", "", err
+	}
+	secrets, err := kubClient.CoreV1().Secrets().Get(onostest.SecretsName)
+	if err != nil {
+		return "", "", err
+	}
+	username := string(secrets.Object.Data["sd-ran-username"])
+	password := string(secrets.Object.Data["sd-ran-password"])
+
+	return username, password, nil
+}
+
 // CreateSdranRelease creates a helm release for an sd-ran instance
 func CreateSdranRelease() (*helm.HelmRelease, error) {
+	username, password, err := getCredentials()
+	if err != nil {
+		return nil, err
+	}
+
 	sdran := helm.Chart("sd-ran", onostest.SdranChartRepo).
 		Release("sd-ran").
+		SetUsername(username).
+		SetPassword(password).
 		Set("import.onos-config.enabled", false).
 		Set("import.onos-topo.enabled", false).
 		Set("onos-e2t.image.tag", "latest").
@@ -28,9 +51,13 @@ func CreateRanSimulator() *helm.HelmRelease {
 
 // CreateRanSimulatorWithName creates a ran simulator
 func CreateRanSimulatorWithName(name string) *helm.HelmRelease {
+	username, password, _ := getCredentials()
+
 	simulator := helm.
 		Chart(name, onostest.SdranChartRepo).
 		Release(name).
+		SetUsername(username).
+		SetPassword(password).
 		Set("image.tag", "latest")
 	return simulator
 }
