cc97b7432c254fbf35d329576a104e1bbd8ffd33
Thomas Vachuska <tom@opennetworking.org>
2021-02-19 11:09:41 -0800

    Removed unnecessary methods from the Model

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -63,10 +63,7 @@ type Cell struct {
 	Neighbors []types.ECGI `yaml:"neighbors"`
 	TxPowerDB float64      `yaml:"txPower"`
 
-	// TODO: should not be needed as it coincides with sector center.
-	//Location  Coordinate `yaml:"location"`
-
-	// TODO: add the following later or track them differently
+	// TODO: track tbhe following relationships differently via UERegistry
 	//Crntis map
 	//CrntiIndex uint32     `yaml:"crntiIndex"`
 	//Port       uint32     `yaml:"port"`
@@ -104,32 +101,12 @@ type ServiceModel struct {
 	Version     string `yaml:"version"`
 }
 
-// GetNode gets a an e2 node
-func (m *Model) GetNode(name string) (Node, error) {
-	if node, ok := m.Nodes[name]; ok {
-		return node, nil
-	}
-
-	return Node{}, errors.New(errors.NotFound, "node not found")
-}
-
-// GetServiceModel gets a service model  based on a given name
+// GetServiceModel gets a service model based on a given name.
 func (m *Model) GetServiceModel(name string) (ServiceModel, error) {
 	if sm, ok := m.ServiceModels[name]; ok {
 		return sm, nil
 	}
-
 	return ServiceModel{}, errors.New(errors.NotFound, "the service model not found")
-}
-
-// GetNodes gets all of the simulated nodes
-func (m *Model) GetNodes() []Node {
-	nodes := make([]Node, 0, len(m.Nodes))
-	for _, node := range m.Nodes {
-		nodes = append(nodes, node)
-	}
-
-	return nodes
 }
 
 // GetController gets a controller by a given name
@@ -137,16 +114,5 @@ func (m *Model) GetController(name string) (Controller, error) {
 	if controller, ok := m.Controllers[name]; ok {
 		return controller, nil
 	}
-
 	return Controller{}, errors.New(errors.NotFound, "controller not found")
-}
-
-// GetControllers gets all of the controllers
-func (m *Model) GetControllers() []Controller {
-	var controllers []Controller
-	for _, controller := range m.Controllers {
-		controllers = append(controllers, controller)
-	}
-
-	return controllers
 }
