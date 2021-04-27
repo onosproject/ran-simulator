198cde96669ab1b3a77725fcc2160291c356b7c5
Thomas Vachuska <tom@opennetworking.org>
2021-04-26 17:38:11 -0700

    Took out the partially completed test

--- pkg/mobility/driver_test.go
+++ pkg/mobility/driver_test.go
@@ -5,25 +5,8 @@
 package mobility
 
 import (
-	"context"
-	"github.com/onosproject/ran-simulator/pkg/model"
-	"github.com/onosproject/ran-simulator/pkg/store/cells"
-	"github.com/onosproject/ran-simulator/pkg/store/nodes"
-	"github.com/onosproject/ran-simulator/pkg/store/routes"
-	"github.com/onosproject/ran-simulator/pkg/store/ues"
 	"testing"
 )
 
 func TestDriver(t *testing.T) {
-	m := &model.Model{}
-	err := model.LoadConfig(m, "test")
-
-	ns := nodes.NewNodeRegistry(m.Nodes)
-	cs := cells.NewCellRegistry(m.Cells, ns)
-	us := ues.NewUERegistry(1, cs)
-	rs := routes.NewRouteRegistry()
-
-	ctx := context.Background()
-
-
 }
