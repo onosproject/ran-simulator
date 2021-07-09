6d552481f37369b35bba2e6ff484d219ece8d09c
Francis Chua <francis@opennetworking.org>
2021-07-08 17:23:32 -0700

    temp

---
+++ tests/ransim/suite.go
@@ -0,0 +1,32 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+
+package ransim
+
+import (
+	"github.com/onosproject/helmit/pkg/input"
+	"github.com/onosproject/helmit/pkg/test"
+	"github.com/onosproject/ran-simulator/tests/utils"
+)
+
+// TestSuite is the primary ran-simulator test suite
+type TestSuite struct {
+	test.Suite
+}
+
+// SetupTestSuite sets up the ran-simulator test suite
+func (s *TestSuite) SetupTestSuite(c *input.Context) error {
+	sdran, err := utils.CreateSdranRelease(c)
+	if err != nil {
+		return err
+	}
+	err = sdran.Install(true)
+	if err != nil {
+		return err
+	}
+
+	// Create an instance of the simulator
+	simulator := utils.CreateRanSimulatorWithName(c, "ran-simulator")
+	return simulator.Install(true)
+}
