313e092d9c9f307324bca7faa13e9ca3b4c04239
Ray Milkey <ray@opennetworking.org>
2021-10-05 11:16:00 -0700

    Update license refs to LicenseRef-ONF-Member-Only-1.0

--- pkg/utils/measurement/tttrange.go
+++ pkg/utils/measurement/tttrange.go
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
 //
-// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+// SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
 
 package utils
 
