a16e4c3f6ca78467fb90cf69ddf151e2e3b7392c
Ray Milkey <ray@opennetworking.org>
2022-02-03 09:38:35 -0800

    Change licenses to Apache 2.0

--- tests/e2t/suite.go
+++ tests/e2t/suite.go
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
 //
-// SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
+// SPDX-License-Identifier: Apache-2.0
 
 package e2t
 
